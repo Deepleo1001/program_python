9ea6020acc35607ff514f741543ff1cc4c5e4dc4
Deepleo1001 <hudaipiwardani@gmail.com>
2022-01-23 03:36:34 +0800

    menambah program shortlink

---
+++ shortlink.py
@@ -0,0 +1,7 @@
+import pyshorteners
+
+deep_token = pyshorteners.Shortener(api_key="3ba8189edc97d6590a14dfb8601a1da0141a18c0")
+link=input("Masukkan Link \t")
+link=bitly.clicks
+hasil_link=deep_token.bitly.linkclicks(link)
+print(hasil_link)
